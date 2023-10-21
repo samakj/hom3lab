cabfb5a9483df2ad206fe767b879e6fb02f7756a
sam <samakj@live.co.uk>
2023-10-21 12:05:02 +0100

    fix: Add auth db user with relevant priveledges

--- services/postgres/patches/templates/0002.create_users.template.sql
+++ services/postgres/patches/templates/0002.create_users.template.sql
@@ -1,1 +1,2 @@
 CREATE USER {secrets.api.username} WITH PASSWORD '{secrets.api.password}';
+CREATE USER {secrets.authorisation.username} WITH PASSWORD '{secrets.authorisation.password}';
--- services/postgres/patches/templates/0004.grant_user_priveledges.template.sql
+++ services/postgres/patches/templates/0004.grant_user_priveledges.template.sql
@@ -6,3 +6,6 @@ GRANT USAGE ON SCHEMA {db.schema.iot.name} TO {secrets.api.username};
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA {db.schema.iot.name} TO {secrets.api.username};
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA {db.schema.iot.name} TO {secrets.api.username};
 
+GRANT USAGE ON SCHEMA {db.schema.authorisation.name} TO {secrets.authorisation.username};
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA {db.schema.authorisation.name} TO {secrets.authorisation.username};
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA {db.schema.authorisation.name} TO {secrets.authorisation.username};
